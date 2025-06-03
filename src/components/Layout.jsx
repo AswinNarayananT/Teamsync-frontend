15c09090db096df9350cb9826e3566645ae8b552
aswinnt <aswinmalamakkavu@gmail.com>
2025-06-03 10:47:27 +0530

    corrected the imported file name

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import Sidebar from "./Sidebar";
+import Sidebar from "./SideBar";
 import TopNavigation from "./TopNavigation";
 
 const Layout = ({ role, activeSection, setActiveSection, children }) => {
